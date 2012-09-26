c0594b596564a5afef8ac375b6c2d5918f97489e
florianhervouet <fhervouet@gmail.com>
2012-09-26 22:57:00 +0200

    TPV&V tests unitaires et plan d'integration

---
+++ fr.univ-rennes1.auction4student/src/test/UserImplTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import auction.impl.UserImpl;
+import junit.framework.TestCase;
+
+public class UserImplTest extends TestCase {
+
+	private UserImpl u;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		u = new UserImpl("firstname", "lastname", "email", "password", "address");
+	}
+
+	public void testGetAdress() {
+		assertEquals("Test de getAdress()", "address", u.getAdress());
+	}
+
+	public void testGetEmail() {
+		assertEquals("Test de getAdress()", "email", u.getEmail());
+	}
+
+	public void testGetFirstName() {
+		assertEquals("Test de getAdress()", "firstname", u.getFirstName());
+	}
+
+	public void testGetLastName() {
+		assertEquals("Test de getAdress()", "lastname", u.getLastName());
+	}
+
+	public void testGetPassword() {
+		assertEquals("Test de getAdress()", "password", u.getPassword());
+	}
+
+}
