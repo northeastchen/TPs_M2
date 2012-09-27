93ffc52a44f00a18c1c4a42b1b3e70b9841759a1
JGuillet <julienguillet56@gmail.com>
2012-09-27 23:02:16 +0200

    BidImplTest

--- fr.univ-rennes1.auction4student/src/test/BidImplTest.java
+++ fr.univ-rennes1.auction4student/src/test/BidImplTest.java
@@ -1,27 +1,59 @@
 package test;
 
+import java.util.List;
+
 import junit.framework.TestCase;
+
+import org.easymock.EasyMock;
+
+import auction.Auction;
+import auction.impl.AuctionImpl;
+import auction.impl.BidImpl;
+import auction.impl.UserImpl;
 
 public class BidImplTest extends TestCase {
 
+	private UserImpl user;
+	private Auction mockAuction;
+	
+	private BidImpl bid;
+	private BidImpl bid2;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
+		user = new UserImpl("firstname","lastname","email","password","address");
+	
+		mockAuction = EasyMock.createMock(AuctionImpl.class);
+		
+		List<BidImpl> bids = new java.util.ArrayList<BidImpl>();
+		EasyMock.expect(mockAuction.getBids()).andReturn(bids);
+		EasyMock.expect(mockAuction.getBids()).andReturn(bids);
+		EasyMock.replay(mockAuction);
+		bid = new BidImpl(user, mockAuction, 12345);
+	}
+	
+	public void testBidImpl() {
+		bid2 = new BidImpl(user, mockAuction, 0);
+		assertNotNull(bid2);
+		assertEquals(0, bid2.getAmount());
+		assertEquals(user, bid2.getUser());
+		assertEquals(mockAuction, bid2.getAuction());
 	}
 
 	public void testGetAmount() {
-		fail("Not yet implemented");
+		assertEquals(12345, bid.getAmount());
 	}
 
 	public void testGetAuction() {
-		fail("Not yet implemented");
+		assertEquals(mockAuction, bid.getAuction());
 	}
 
 	public void testGetUser() {
-		fail("Not yet implemented");
+		assertEquals(user, bid.getUser());
 	}
 
 	public void testToString() {
-		fail("Not yet implemented");
+		assertEquals(user.getEmail() + " " + 12345, bid.toString());
 	}
 
 }
