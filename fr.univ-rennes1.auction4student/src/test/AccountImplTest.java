e4dc2c0d923e97b89c2d4db76d34c5b94e3de8d8
florianhervouet <fhervouet@gmail.com>
2012-10-01 23:16:29 +0200

    Debut test account

--- fr.univ-rennes1.auction4student/src/test/AccountImplTest.java
+++ fr.univ-rennes1.auction4student/src/test/AccountImplTest.java
@@ -1,18 +1,51 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
+
+import org.easymock.EasyMock;
+
+import auction.Auction;
 import auction.User;
 import auction.impl.AccountImpl;
+import auction.impl.AuctionImpl;
+import auction.impl.BidImpl;
 import auction.impl.UserImpl;
 
 public class AccountImplTest extends TestCase {
 
 	AccountImpl testA;
+	Auction mockau1;
+	Auction mockau2;
+	List<Auction> l_auction;
+	
+	BidImpl bid1;
+	BidImpl bid2;
+	BidImpl bid3;
+	
+	UserImpl u1;
+	UserImpl u2;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		User u = new UserImpl("firstname", "lastname", "email", "password", "address");
-		testA = new AccountImpl(u);
+		u1 = new UserImpl("firstname", "lastname", "email", "password", "address");
+		u2=new UserImpl("firstname2","lastname2","email2","password2","address2");
+		
+		testA = new AccountImpl(u1);
+		
+		mockau1=EasyMock.createMock(AuctionImpl.class);
+		mockau2=EasyMock.createMock(AuctionImpl.class);
+		
+		
+		bid1=new BidImpl(u1, mockau1, 20);
+		bid2=new BidImpl(u1, mockau2, 10);
+		bid3=new BidImpl(u2, mockau2, 40);
+		//EasyMock.expect(mockau1.getMaxBid()).andReturn(messages).anyTimes(); 
+		
+		l_auction=new ArrayList<Auction>();
+		
 	}
 	
 	public void testAccountImpl() {
@@ -33,8 +66,13 @@ public class AccountImplTest extends TestCase {
 		assertEquals(-50, testA.getCredit());
 	}
 
-	public void testGetFreeCredit() {
-		fail("Not yet implemented");
+	public void testGetFreeCredit1() {
+		//passe 4 fois dans la boucle 
+		
+		l_auction.add(mockau1);
+		l_auction.add(mockau2);
+		
+		
 	}
 
 	public void testGetCredits() {
